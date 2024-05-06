aee77419a7a90ebba9b8d5e9bb7b151117cf43c0
JoseAlmendras <josealmendras779@gmail.com>
2024-05-05 22:37:13 -0400

    se agrega comentario de este archivo

--- ejercicioNegocio/negocio.py
+++ ejercicioNegocio/negocio.py
@@ -4,7 +4,12 @@ import os
 os.system('cls')
 
 dicJuego = juegos._juegos
+""" 
 
+el codigo extrae datos del diccionario creado en el archivo anterior, en este lo que hacemos es a traves de algoritmos extraer los
+los datos que nesesitemos y que nos aparescan solo los que queramos en pantalla.
+
+"""
 #tipo = "Pc"
 
 #dicPc = dicJuego[tipo]
